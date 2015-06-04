87c142ffedae0044f6d9737073def82bb6f9befc
teonapster <teonapster@gmail.com>
2015-06-04 19:57:21 +0300

    Initial vakspam commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vakspamApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vakspamApp
+ */
+angular.module('vakspamApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vakspamApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vakspamApp
+ */
+angular.module('vakspamApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
