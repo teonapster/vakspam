fa8475b4b5be3beab1fe3393325c76103d553610
teonapster <teonapster@gmail.com>
2015-06-06 00:12:46 +0300

    Random Reviews with fake backend

--- app/scripts/fakeHttp.js
+++ app/scripts/fakeHttp.js
@@ -4,5 +4,8 @@ angular
 .module('fakeHttp',['vakspamApp','ngMockE2E'])
 .run(function($httpBackend,randVal){
     $httpBackend.whenGET(/reviews/).respond(randVal.randReviews(50));
+    
+    //Ignore all views
     $httpBackend.whenGET(/views\/.*/).passThrough();
+    
 });
